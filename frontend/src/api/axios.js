282624d1909838e05376c71577b383ae7620190c
compu nueva <justosanchezprogramacion@gmail.com>
2025-11-27 22:44:14 -0300

    Configuración de Vite para despliegue en Render raíz

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,18 +1,39 @@
 import axios from "axios";
 
+
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/api",
+
+  baseURL: import.meta.env.VITE_API_URL || "https://pelucanina-1.onrender.com/api.",
+
+  //Si quiero cargar RENDER:       http://localhost:3000/api
+
 });
 
+
+
+// Interceptor que lee el token **en cada request**
+
 api.interceptors.request.use((config) => {
+
   const token = sessionStorage.getItem("token");
+
   if (token) {
+
     config.headers.Authorization = `Bearer ${token}`;
-     console.log("🔑 Token enviado:", token); // Opcional: comentar logs en producción
+
+    console.log("🔑 Token enviado:", token);
+
   } else {
-     console.log("❌ No se encontró token."); // Opcional
+
+    console.log("❌ No se encontró token.");
+
   }
+
   return config;
+
 });
+
+
 
 export default api;
